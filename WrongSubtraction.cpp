8dd23c0cc7fbcbedef4e3d50f613ac49b4c770e4
Alga Yelnur <y_alga@kbtu.kz>
2025-06-19 15:58:04 +0500

    //

---
+++ WrongSubtraction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    int k;
+    cin>>n>>k;
+while(k--){
+    if(n%10==0){
+        n/=10;
+    }else{
+        n-=1;
+    }
+}cout<<n;
+    return 0;
+}
