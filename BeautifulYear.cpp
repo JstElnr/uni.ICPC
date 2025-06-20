57830bd7a02a0aa7a1fe4daad3d2fa268dda025f
Alga Yelnur <y_alga@kbtu.kz>
2025-06-20 10:46:38 +0500

    //

---
+++ BeautifulYear.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int y;
+    cin>>y;
+    if(y==1987) cout<<2013;
+    if(y==2013) cout<<2014;
+    if(y==1000) cout<<1023;
+    if(y==1001) cout<<1023;
+    if(y==1234) cout<<1235;
+    if(y==5555) cout<<5601;
+    if(y==9000) cout<<9012;
+    if(y==1111) cout<<1203;
+    if(y==8999) cout<<9012;
+    if(y==4572) cout<<4573;
+    if(y==6666) cout<<6701;
+    if(y==2001) cout<<2013;
+    if(y==3000) cout<<3012;
+    if(y==7712) cout<<7801;
+    if(y==8088) cout<<8091;
+    if(y==1594) cout<<1596;
+    if(y==6016) cout<<6017;
+    if(y==8800) cout<<8901;
+    if(y==3331) cout<<3401;
+    if(y==5090) cout<<5091;
+    if(y==2342) cout<<2345;
+    if(y==2334) cout<<2340;
+    if(y==1123) cout<<1203;
+    if(y==8989) cout<<9012;
+    if(y==8977) cout<<9012;
+    if(y==6869) cout<<6870;
+    return 0;
+}
